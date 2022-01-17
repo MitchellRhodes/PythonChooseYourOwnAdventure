858a4d8693e2897c8a9dedf10d82dd9ac747d05c
Mitch Rhodes <M.Rhodes87@comcast.net>
2022-01-16 22:22:30 -0500

    Added a player class. Refactored to separate methods for main, starting_area, and eye_decision.

---
+++ Player.py
@@ -0,0 +1,9 @@
+class Player:
+
+    def __init__ (self, name, eyesight, action_count, is_alive, inventory, location):
+        self.name = name
+        self.eyesight = eyesight
+        self.action_count = action_count
+        self.is_alive = is_alive
+        self.inventory = inventory
+        self.location = location
--- chooseYourOwnAdventure.py
+++ chooseYourOwnAdventure.py
@@ -1,40 +1,93 @@
-#Movement direction is crucial here – you must create walls and set the directions in which the users can move through the rooms, set movement restrictions, 
-# and also include a tracker that can track how far a user has walked or moved in the game. Mentioning Python projects can help your resume look much more interesting 
-# than others.
+from Player import Player
 
-play_game = input("Would you like to play? (yes/no)")
 
-if play_game.lower().strip() == "yes":
-
-    eyesight = None
-    action_count = 0
-    direction = ""
-
+def main():
+    User.location = "start"
 
     print("You open your eyes. Your surroundings are blurry.")
     
-    while eyesight == None:
-        eye_decision = input("Will you blink furiously or rub your eyes? (blink/rub)")
-        if eye_decision.lower().strip() == "blink":
-            print("It did little to improve your eyesight...")
-            eyesight = 0
-            action_count += 1
-        elif eye_decision.lower().strip() == "rub":
-            print("Your eyesight has improved remarkably!")
-            eyesight = 1
-            action_count += 1
-        else:
-            print("Could you repeat that?")
+    eye_decision()
 
     print("You stand up and survey the room.")
+    starting_area()
+
+
+    if User.location == "start":
+
+        starting_area()
+    elif User.location == "mirror":
+
+        print("You walk up to the mirror")
+    elif User.location == "window":
+
+        print("You walk over to the window")
+    elif User.location == "door":
+        
+        print("You walk towards the door.")
+
+
+
+
+def starting_area():
+
+    direction = ""
+
     print("Behind you is a wall. In front of you is a mirror. To the left is a door. To the right is a window.")
 
     direction = input("Which way will you head? (back/forward/left/right)")
 
     if direction.lower().strip() == "back":
-        print("That's a wall...")
-        action_count += 1
-             
 
+        print("That's a wall... So, you turn back to find a new path")
+        User.location = "start"
+        User.action_count += 1
+        return
+    elif direction.lower().strip() == "forward":
+
+        User.location = "mirror"
+        User.action_count += 1
+        return
+    elif direction.lower().strip() == "left":
+
+        User.location = "door"
+        User.action_count += 1
+        return
+    elif direction.lower().strip() == "right":
+
+        User.location = "window"
+        User.action_count += 1
+        return
+
+
+def eye_decision():
+    while User.eyesight == None:
+
+        eye_decision = input("Will you blink furiously or rub your eyes? (blink/rub)")
+
+        if eye_decision.lower().strip() == "blink":
+
+            print("It did little to improve your eyesight...")
+            User.eyesight = 0
+            User.action_count += 1
+        elif eye_decision.lower().strip() == "rub":
+
+            print("Your eyesight has improved remarkably!")
+            User.eyesight = 1
+            User.action_count += 1
+        else:
+
+            print("Could you repeat that?")
+
+
+#==============================================================
+play_game = input("Would you like to play? (yes/no)")
+
+if play_game.lower().strip() == "yes":
+
+    name = input("What is your first name?")
+    User = Player(name, None, 0, True, [], "Start")
+
+    main()
+             
 else:
     print("Maybe another time")
