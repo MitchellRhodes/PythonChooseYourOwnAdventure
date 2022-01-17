0bf14ea83b45d2fe15e135ad8027377bb283b2f2
Mitch Rhodes <M.Rhodes87@comcast.net>
2022-01-16 21:33:44 -0500

    Initial commit.

---
+++ chooseYourOwnAdventure.py
@@ -0,0 +1,13 @@
+#Movement direction is crucial here – you must create walls and set the directions in which the users can move through the rooms, set movement restrictions, 
+# and also include a tracker that can track how far a user has walked or moved in the game. Mentioning Python projects can help your resume look much more interesting 
+# than others.
+
+play_game = input("Would you like to play? (yes/no)")
+
+if play_game.lower().strip() == "yes":
+
+    print("play game")
+             
+
+else:
+    print("Maybe another time")
